546264f32d05b9be1f88e1c338fd7c0c4d780921
EthanKim <hey0507@gmail.com>
2023-03-20 14:42:10 +0900

    [Init] Create Project

---
+++ src/main/java/jep/project/beyondcloset/BeyondClosetApplication.java
@@ -0,0 +1,11 @@
+package jep.project.beyondcloset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondClosetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BeyondClosetApplication.class, args);
+    }
+}
---
+++ src/test/java/jep/project/beyondcloset/BeyondclosetApplicationTests.java
@@ -0,0 +1,13 @@
+package jep.project.beyondcloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondclosetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
